1b62e204923bd1eed4517a1972742a3eb9d18c8a
louisquincy <89979832+louisquincy@users.noreply.github.com>
2026-03-24 04:47:26 +0700

    Update image_proxy.cpp

--- lab02/image_proxy.cpp
+++ lab02/image_proxy.cpp
@@ -5,17 +5,14 @@
 ImageProxy::ImageProxy(const QString& filepath)
     : filepath_(filepath), realImage_(nullptr)
 {
-    // Конструктор МГНОВЕННЫЙ. Ничего не грузим.
     qDebug() << "[Proxy] Created for:" << QFileInfo(filepath_).fileName();
 }
 
 QPixmap ImageProxy::getPixmap(int width, int height) {
-    // ЛЕНИВАЯ ИНИЦИАЛИЗАЦИЯ: RealImage создаётся только при первом вызове
     if (!realImage_) {
         qDebug() << "[Proxy] First access -> creating RealImage...";
         realImage_ = std::make_unique<RealImage>(filepath_);
     }
-    // Делегируем работу реальному объекту
     return realImage_->getPixmap(width, height);
 }
 
